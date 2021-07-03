f6a4a5287128faab3c1168ce824641a0143edc9b
eLBati <lb@takobi.online>
2021-07-03 06:43:38 +0200

    setup

--- newrelic/__manifest__.py
+++ newrelic/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'NewRelic Instrumentation',
     'description': 'Wraps requests etc.',
-    'version': '1.0',
+    'version': '12.0.1.0.0',
     'website': 'https://hibou.io/',
     'author': 'Hibou Corp. <hello@hibou.io>',
     'license': 'AGPL-3',
